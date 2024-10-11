eaf000fce0ce8bfa62f6fa239299dc10652ef3d9
Francisco Varela <franxvarela@yahoo.es>
2024-10-11 11:36:09 +0200

    Enable databricks-connect for delta_table_poc

--- app/delta_table_poc.py
+++ app/delta_table_poc.py
@@ -2,13 +2,15 @@ from pyspark.sql import SparkSession
 from delta import configure_spark_with_delta_pip
 from pyspark.sql.functions import lit, rand, concat, col
 
-def create_spark_session(app_name="DeltaTablePOC"):
-    builder = SparkSession.builder.appName(app_name) \
-        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-    
-    return configure_spark_with_delta_pip(builder).getOrCreate()
-
+def get_spark() -> SparkSession:
+  try:
+    from databricks.connect import DatabricksSession
+    print("Databricks environment detected")
+    return DatabricksSession.builder.getOrCreate()
+  except:
+    print("Local environment detected")
+    return SparkSession.builder.getOrCreate()
+  
 def create_sample_dataframe(spark, num_rows=10):
     return spark.range(num_rows) \
         .withColumn("name", concat(lit("Person_"), col("id").cast("string"))) \
@@ -26,16 +28,11 @@ def main():
     writes it to a Delta table, reads it back, and displays the results.
     """
 
-    spark = create_spark_session()
-    
+    spark = get_spark()
     data = create_sample_dataframe(spark)
-    
     delta_table_path = "tmp/delta_table_poc"
-    
     write_delta_table(data, delta_table_path)
-    
     print(f"Delta table created at: {delta_table_path}")
-    
     read_df = read_delta_table(spark, delta_table_path)
     read_df.show(5)
     
